6cbb772857cd0cf270712e140f3edcef656e562a
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-07-03 00:43:38 +0530

    defined components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,17 @@
-import '@styles/globals.css'
-import { Children } from 'react'
+import "@styles/globals.css";
+import { Children } from "react";
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
-            <main className='app'>
-                { children }
-            </main>
-        </body>
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
