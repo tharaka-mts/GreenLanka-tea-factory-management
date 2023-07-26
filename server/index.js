96790d7e097533b5a5d03de12eb0f56311214314
Yureni-vr <ureni.vidunika@gmail.com>
2023-07-26 20:57:41 +0530

    reg

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ import express from "express";
 const app = express();
 const port = 3005;
 
+
+
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/register", (req, res) => {
+  res.send("Reg request!");
 });
 
 app.listen(port, () => {
