eac60b8fcfa663d80539c3f8f9916286db3b5f3a
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-07-28 22:09:46 +0530

    pages added

---
+++ client/src/pages/Attendance.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Attendance = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <h1>Attendance</h1>
+    </div>
+  )
+}
+
+export default Attendance
---
+++ client/src/pages/Production.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Production = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <h1>Production</h1>
+    </div>
+  )
+}
+
+export default Production
