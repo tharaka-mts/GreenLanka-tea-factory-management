83fb843d7de349a932e62c3f2a48b28fbaff9785
Yureni Ranathunga <ureni.vidunika@gmail.com>
2023-08-14 14:59:16 +0530

    updated

--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -20,4 +20,65 @@ router.post('/register', async(req, res) => {
     res.json({ message: "User registered successfully!"});
 });
 
+router.get('/users/:id', async (req, res) => {
+    try {
+      const user = await userModel.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+      res.status(500).json({ message: 'Error fetching user details' });
+    }
+  });
+
+  router.put('/users/:id', async (req, res) => {
+    try {
+      const { firstname, lastname, username, type, email, address, mobile, password } = req.body;
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const updatedUser = await userModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          firstname,
+          lastname,
+          username,
+          type,
+          email,
+          address,
+          mobile,
+          password: hashedPassword,
+        },
+        { new: true }
+      );
+  
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.json({ message: 'User updated successfully!', user: updatedUser });
+    } catch (error) {
+      console.error('Error updating user:', error);
+      res.status(500).json({ message: 'Error updating user' });
+    }
+  });
+  
+  router.delete('/users/:id', async (req, res) => {
+    try {
+      const deletedUser = await userModel.findByIdAndDelete(req.params.id);
+  
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.json({ message: 'User deleted successfully!' });
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      res.status(500).json({ message: 'Error deleting user' });
+    }
+  });
+
+
 export { router as userRouter };
