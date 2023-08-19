8d5d16f978a3201edbf5fe96d3a79b6963d8194a
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-08-20 00:49:21 +0530

    frontend - bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -109,10 +109,6 @@ const App = () => {
           element: <AddUser />,
         },
         {
-          path: "/login",
-          element: <Login />,
-        },
-        {
           path: "/AddSalaryDetails",
           element: <AddSalaryDetails />,
         },
@@ -144,8 +140,6 @@ const App = () => {
           path: "/ViewWeight",
           element: <ViewWeight />,
         },
-
-
       ],
     },
     {
