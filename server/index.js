b9bd0ab66f2c9297fa45cc9fbfefd6b8a3222326
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-08-28 12:42:15 +0530

    backend - update attendace endpoint

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 import { userRouter } from "./src/routes/userRouter.js";
 import { getDetailsRouter } from "./src/routes/getDetails.js";
+import { userAttendanceRouter } from "./src/routes/userAttendance.js";
 
 const app = express();
 const port = 3005;
@@ -17,10 +18,12 @@ app.use(cors());
 async function startServer() {
     try {
         await mongoose.connect(process.env.MONGODB_CLOUD_URI);
+        // await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB");
 
         app.use('/auth', userRouter);
         app.use('/get', getDetailsRouter);
+        app.use('/attendance', userAttendanceRouter);
 
         app.listen(port, () => {
             console.log(`Listening on port ${port}`);
