04a34558fdfd10328b14f45e94079b4f5c8325fc
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-08-28 12:12:51 +0530

    backend - attendance model

--- server/src/models/attendance.js
+++ server/src/models/attendance.js
@@ -1,10 +1,12 @@
 import { Schema, model } from 'mongoose';
 
 const attendanceSchema = new Schema({
-    userId: {type: String, required: true},
+    userId: {type: Schema.Types.ObjectId, ref:'users', required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
     date: {type: Date, required: true},
-    inTime: {type: Date, required: true},
-    outTime: {type: Date, required: true},
+    inTime: {type: String, required: true},
+    outTime: {type: String, required: true},
 });
 
-export const attendanceModel = model('attendance', attendanceSchema);
+export const attendanceModel = model('attendances', attendanceSchema);
