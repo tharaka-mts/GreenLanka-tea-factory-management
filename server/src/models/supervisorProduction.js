a61d72fe680add7144ef4df8c14c65213c2791f9
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-09-04 13:45:10 +0530

    backend - supervisor production model

---
+++ server/src/models/supervisorProduction.js
@@ -0,0 +1,13 @@
+import { Schema, model } from 'mongoose';
+
+const supProductionSchema = new Schema({
+    userId: {type: Schema.Types.ObjectId, ref:'users', required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    date: {type: Date, required: true},
+    time: {type: String, required: true},
+    weight: {type: Number, required: true},
+    comment: {type: String, required: true},
+});
+
+export const supProductionModel = model('supProduction', supProductionSchema);
