dfe2f8b2be5b133e98aea93438b8539aab345e8c
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-09-08 17:35:01 +0530

    backend - teams model

---
+++ server/src/models/teams.js
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+
+const teamsSchema = new Schema({
+    supId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+    supFirstname: { type: String, required: true },
+    supLastname: { type: String, required: true },
+    team: [
+        {
+            userId: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+            firstname: { type: String, required: true },
+            lastname: { type: String, required: true },
+            nic: { type: String, required: true },
+        }
+    ],
+});
+
+export const teamsModel = model('teams', teamsSchema);
