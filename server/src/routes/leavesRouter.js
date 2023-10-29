7b8ddd77a0453aa48eb65ec89537e8118aec24ac
Ovindu Jeewanga Hathnapitiya <ovinduuu@gmail.com>
2023-10-29 13:54:58 +0530

    Create leavesRouter.js

---
+++ server/src/routes/leavesRouter.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { leaveModel } from '../models/leaves.js';
+
+const router = express.Router();
+
+// Fetch all leaves details
+router.get('/getLeaves', (req, res) => {
+    leaveModel.find()
+    .then(leave => {
+        res.json(leave);
+    })
+    .catch(err => {
+        console.error('Error fetching leave:', err);
+        res.status(500).json({ error: 'An error occurred while fetching leave.' });
+    });
+});
+
+// Create new leave
+router.post('/createLeave', async (req, res) => {
+    try {
+        const { username, date, type, reason } = req.body;
+
+        const newLeave = new leaveModel({
+            username,
+            date,
+            type,
+            reason
+        });
+
+        const savedLeave = await newLeave.save();
+        res.json(savedLeave);
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+export { router as leavesRouter };
