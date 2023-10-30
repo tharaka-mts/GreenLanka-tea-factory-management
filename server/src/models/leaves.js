d5d52381c1960d69d8037b0a0cc9715c2ae81dbe
Ovindu Jeewanga Hathnapitiya <ovinduuu@gmail.com>
2023-10-30 14:04:48 +0530

    Update leaves.js

--- server/src/models/leaves.js
+++ server/src/models/leaves.js
@@ -2,9 +2,11 @@ import { Schema, model } from 'mongoose';
 
 const leaveSchema = new Schema({
     username: {type: String, required: true},
-    date: {type: String, required: true},
+    startdate: {type: String, required: true},
+    enddate: {type: String, required: true},
     type: {type: String, required: true},
-    reason: {type: String, required: true}
+    reason: {type: String, required: true},
+    status: {type: String, required: true}
 });
 
 export const leaveModel = model('leave', leaveSchema);
